c6f001e7a44761209397e9278d2d152a3a1b9b64
Moldee81 <a.penatomczak@gmail.com>
2025-09-18 07:37:13 +0100

    Created Dictionary component with a search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center">
+          {" "}
+          Coded by <span className="accent">Anita Peña-Tomczak</span>
+        </footer>
+      </div>
     </div>
   );
 }
