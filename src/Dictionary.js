02f95763fccba2d926e2c62ed3285a43114b1286
Moldee81 <a.penatomczak@gmail.com>
2025-09-26 11:23:22 +0100

    Results  & Meaning components added to the project

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
+    setResults(response.data.meanings[0]);
   }
-
   function search(event) {
     event.preventDefault();
 
@@ -24,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
